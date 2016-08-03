f0ca5f86c3d83380ee99e7a3e53e09de0516e93f
kokila550 <kokila550@gmail.com>
2016-08-03 10:37:05 +0530

    Navigate to Dashbard after login Added

---
+++ RideshareAdmin.DBAccess/RideshareAdmin.Console/ServiceWrapper/ServiceWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace RideshareAdmin.Console.ServiceWrapper
+{
+    public class ServiceWrapper : IServiceWrapper
+    {
+        public async Task<string> LoginUser(string username, string password)
+        {
+            using (var client = new HttpClient())
+            {
+
+                var result = await client.GetAsync("http://localhost:49604/Applicaion_Service/RideshareService.svc/LoginUser/" + username + "/" + password + "/");
+                return await result.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
