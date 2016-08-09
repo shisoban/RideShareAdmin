83240ced67a12055c3c34b44b698a987d8e8ee66
AshJayakody <ashajayakody@gmail.com>
2016-08-09 09:51:38 +0530

    TotalDistance controller and business entity added

---
+++ RideshareAdmin.DBAccess/RideshareAdmin.WebAPI/Controllers/TotalDistanceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RideshareAdmin.Services;
+using BusinessEntities;
+
+namespace RideshareAdmin.WebAPI.Controllers
+{
+    public class TotalDistanceController : ApiController
+    {
+        public HttpResponseMessage Post()
+        {
+            IRidehistoriesService ridedetail = new RidehistoriesService();
+
+            TotalDistance totalDistance = ridedetail.GetTotalDistance();
+            if (totalDistance != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, totalDistance);
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Error in calculating distance");
+
+        }
+
+        }
+}
